27b94dbb49e09a634e53feb96ed39eaf42f86fe7
huangjinlmf@gmail.com <huangjinlmf@gmail.com>
2024-12-05 16:04:20 +0800

    commit

---
+++ Java技术/多线程/CountDownLatchTest.java
@@ -0,0 +1,46 @@
+package com.huangjin.testthread;
+
+import org.junit.Test;
+
+import java.util.concurrent.*;
+
+/**
+ * Created by huang on 2018-11-14.
+ */
+public class CountDownLatchTest {
+
+    private static final int THREAD_POOL_CORE_SIZE = 5;// 线程池最少线程数
+    private static final int THREAD_POOL_MAX_SIZE = 20;// 最大线程数
+    private static final int THREAD_MAX_THREAD_WAIT = 10000;// 最大线程等待数
+    private static final int THREAD_POOL_WAIT_SECONDS = 5 * 60;// 最长等待时间
+
+    private ExecutorService exec = new ThreadPoolExecutor(THREAD_POOL_CORE_SIZE, THREAD_POOL_MAX_SIZE, THREAD_POOL_WAIT_SECONDS, TimeUnit.SECONDS, new ArrayBlockingQueue<>(THREAD_MAX_THREAD_WAIT), new ThreadPoolExecutor.AbortPolicy());
+
+
+    @Test
+    public void test() {
+        try {
+            CountDownLatch latch = new CountDownLatch(10);
+            for (int threadId = 1; threadId <= 10; threadId++) {
+                this.mession(latch, threadId);
+            }
+            latch.await();
+            System.out.println("结束");
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+
+    public void mession(CountDownLatch latch, Integer threadId) {
+        exec.execute(() -> {
+            try {
+                Thread.sleep(threadId*1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(threadId);
+            latch.countDown();
+        });
+    }
+}
---
+++ Java技术/多线程/CountDownLatch用法.md
@@ -0,0 +1,29 @@
+```java
+//多个子线程同时开启, 等待某个条件完成后, 子线程同时向后执行
+@Test
+public void test193() throws InterruptedException {
+    String key = RandomStringUtils.randomAlphabetic(5);
+    CountDownLatch countDownLatch = new CountDownLatch(50);
+    for (int i = 0; i < 50; i++) {
+        final int j = i;
+        new Thread(() -> {
+            try {
+                countDownLatch.await();
+                Thread.sleep(j);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("线程" + j + "执行!");
+        }).start();
+        countDownLatch.countDown();
+    }
+    Thread.sleep(100000000);
+}
+```
+
+```java
+//主线程等待子线程执行完成后, 继续执行主线程
+/Users/hj_2021/private/Solar-system/Java技术/多线程/CountDownLatchTest.java
+```
